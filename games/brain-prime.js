0e4d342f96f2abdde06989dce00f65c9a8172595
Ruslan Imankulov <rimankulov@thrivedetect.com>
2024-03-05 15:10:25 +0300

    fix typo

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -7,8 +7,8 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function generateGameData() {
   const num = generateRandomNumber(0, MAX_NUM);
-  const isNumberEven = isPrime(num);
-  const correctAnswer = isNumberEven ? 'yes' : 'no';
+  const isNumberPrime = isPrime(num);
+  const correctAnswer = isNumberPrime ? 'yes' : 'no';
   return [`${num}`, correctAnswer];
 }
 
