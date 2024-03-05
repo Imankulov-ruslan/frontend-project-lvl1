422164e4e0b67b4fb7827619494b252c1a1add6d
Ruslan Imankulov <rimankulov@thrivedetect.com>
2024-03-05 11:50:08 +0300

    add cli func

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,27 @@
 import readlineSync from 'readline-sync';
 
-export default function askName() {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
+export function logMessage(str) {
+  console.log(`${str}`);
+}
+
+export function welcomeUser() {
+  logMessage('Welcome to the Brain Games!');
+}
+
+export function askName() {
+  return readlineSync.question('May I have your name? ');
+}
+
+export function greetUser(name) {
+  logMessage(`Hello, ${name}`);
+}
+
+export function congratUser(name) {
+  logMessage(`Congratulations, ${name}`);
+}
+
+export function askQuestion(number) {
+  logMessage('Answer "yes" if the number is even, otherwise answer "no".');
+  logMessage(`Question: ${number}`);
+  return readlineSync.question('Your answer: ');
 }
