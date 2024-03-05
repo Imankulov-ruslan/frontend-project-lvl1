4a7d6cfb5ac643de2a5bb981a399f3bb75602a43
Ruslan Imankulov <rimankulov@thrivedetect.com>
2024-03-05 15:26:48 +0300

    move gcd to numbers helper

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,16 +1,9 @@
 import generateRandomNumber from '../src/numberGenerator.js';
 import gameEngine from '../src/index.js';
+import { findGcd } from '../src/numbers.js';
 
 const MAX_NUM = 1000;
 const rule = 'Find the greatest common divisor of given numbers.';
-
-function findGcd(num1, num2) {
-  while (num1 !== 0 && num2 !== 0) {
-    if (num1 > num2) num1 %= num2;
-    else num2 %= num1;
-  }
-  return num1 + num2;
-}
 
 function generateGameData() {
   const num1 = generateRandomNumber(0, MAX_NUM);
--- src/numbers.js
+++ src/numbers.js
@@ -13,3 +13,11 @@ export function isPrime(number) {
   }
   return true;
 }
+
+export function findGcd(num1, num2) {
+  while (num1 !== 0 && num2 !== 0) {
+    if (num1 > num2) num1 %= num2;
+    else num2 %= num1;
+  }
+  return num1 + num2;
+}
