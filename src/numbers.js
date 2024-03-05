fac0652af21bf0d0c20b1021fe351029aeb5068b
Ruslan Imankulov <rimankulov@thrivedetect.com>
2024-03-05 15:12:25 +0300

    fix isPrime func

--- src/numbers.js
+++ src/numbers.js
@@ -6,7 +6,7 @@ export function isPrime(number) {
   if (number < 2) {
     return false;
   }
-  for (let i = 2; i <= number; i += 1) {
+  for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
     }
