eba8fe60356216d9a46ec6137972b2e321368e8f
Ruslan Imankulov <rimankulov@thrivedetect.com>
2024-03-05 10:49:25 +0300

    eslint fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import askName from '../src/cli.js'
+import askName from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!")
+console.log('Welcome to the Brain Games!');
 
 const userName = askName();
 console.log(`Hello, ${userName}`);
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 export default function askName() {
-    const name = readlineSync.question('May I have your name? ');
-    return name
+  const name = readlineSync.question('May I have your name? ');
+  return name;
 }
